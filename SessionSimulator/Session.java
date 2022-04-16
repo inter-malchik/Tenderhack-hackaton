22fbf28f520ab3fc4abbb4a9975c91e1e2862aa4
Артём Швецов <shvetsovart55@gmail.com>
2022-04-16 17:51:17 +0300

    SessionSimulator v1.1
    changed bets to fix percentage a bet

--- SessionSimulator/Session.java
+++ SessionSimulator/Session.java
@@ -8,37 +8,41 @@ public class Session {
 
     String name;
     public Customer customer;
-    public Vector<Provider> providers;
+    public Vector<Provider> providers = new Vector<Provider>();
     public Provider winner; // or an id of a winner in participants array
 
-    public double maxValue; // max price the customer can offer
-    public double curValue;
+    public double maxValue; // max price
+    public double curValue; // current price
     public double percent; // min percent on which the deal can be lessened
 
     public Session(String name, Customer customer, double maxValue, double percent, long time) {
         startTime = System.currentTimeMillis();
         this.sessionTime = time;
         this.name = name;
+        this.maxValue = maxValue;
         this.curValue = maxValue;
         this.customer = customer;
-        this.maxValue = maxValue;
         this.percent = percent;
         customer.add(this);
     }
 
-    public void add(Provider provider, int curValue) {
-        if (curValue > this.curValue - percent * maxValue / 100 || provider == winner) {
+    public void add(Provider provider) {
+        if (provider == winner) {
             return;
         }
-        if (providers != null && !providers.contains(provider)) {
+        if (!providers.contains(provider)) {
             providers.add(provider);
             provider.add(this);
         }
+        if (curValue - maxValue * percent / 100 < 1) {
+            this.end();
+            return;
+        }
+        curValue -= maxValue * percent / 100;
         winner = provider;
-        this.curValue = curValue;
         if (((startTime + sessionTime * 1000 * 60) - System.currentTimeMillis()) / 1000 / 60 < 1) {
             System.out.println((sessionTime - System.currentTimeMillis()) / 1000 / 60);
-            sessionTime += 1000 * 60; // 1000 ms * 60 = 60s = 1m
+            sessionTime++; // 1000 ms * 60 = 60s = 1m
         }
     }
 
--- SessionSimulator/SessionSimulator.java
+++ SessionSimulator/SessionSimulator.java
@@ -1,6 +1,5 @@
 package SessionSimulator;
 
-import java.time.LocalTime;
 import java.util.Vector;
 
 public class SessionSimulator {
@@ -10,16 +9,25 @@ public class SessionSimulator {
 
     public static void main(String[] args) {
         Customer Ivan = new Customer("Ivan");
+        System.out.println(Ivan.name + " " + Ivan.password);
+
         Provider Alex = new Provider("Alex");
         Provider Leo = new Provider("Leo");
-        Session tender = new Session("IS", Ivan, 100, 1, 2);
+
+        Session tender = new Session("IS", Ivan, 100, 1, 1);
+
         System.out.println(tender.name + " " + tender.customer + " " + tender.curValue);
-        tender.add(Alex, 50);
-        tender.add(Leo, 45);
-        System.out.println(Ivan.name + " " + Ivan.password);
+        tender.add(Alex);
         System.out.println(tender.name + " " + tender.customer + " " + tender.curValue);
+        tender.add(Leo);
+        System.out.println(tender.name + " " + tender.customer + " " + tender.curValue);
+
         while ((System.currentTimeMillis() - tender.startTime) / 1000 / 60 < tender.sessionTime) {}
-        Ivan.del(tender);
+
+        tender.end();
         System.out.println(tender.curValue + " " + tender.winner);
+
+        System.out.println(Alex.historyStats.get(tender));
+        System.out.println(Leo.historyStats.get(tender));
     }
 }
