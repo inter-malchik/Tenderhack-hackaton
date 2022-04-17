ca7d9a62bc68bcce9165103e7269c93dcd1f0ad9
inter-malchik <ipad3007@icloud.com>
2022-04-17 19:55:31 +0300

    Merge branch 'MilaMilaMilaMila-ProductivityAnalysing' into main

---
+++ ProductivityAnalysing/getKoefficient.py
@@ -0,0 +1,23 @@
+from math import exp
+
+
+def find_koefficient(cur_rate, delta_rate, delta_time,  min_total_rate, delta_min_total_rate):
+
+    # нормировка скорости убывания
+    v_total_min_rate = (delta_min_total_rate / min_total_rate) / delta_time
+    v_cur_rate = (delta_rate / cur_rate) / delta_time
+    v_norm = 1 - exp(1 - v_cur_rate / v_total_min_rate)
+
+    # нормировка цены
+    cost_norm = 1 - exp(1 - cur_rate / min_total_rate)
+
+    # коэффициент выгоды цчастия в тендере(рентабельности)
+    k = f(v_norm, cost_norm)
+
+    return round(k, 5)
+
+
+# целевая функция
+def f(v_, cur_rate_):
+    return cur_rate_ * (1 - v_)
+
